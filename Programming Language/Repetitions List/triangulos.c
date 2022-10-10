184e6f12b4c3769979c1ab0604b08f24fa512209
Mauricio Taques <mtaquesfilho@gmail.com>
2022-10-10 18:00:08 -0300

    Add files via upload

---
+++ Programming Language/Repetitions List/triangulos.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main (void){
+
+    int x, y, z, a, b, c;
+
+    scanf("%i %i %i",&x,&y,&z);
+
+    if(x > y)
+    x = a;
+
+    else if(x > z)
+    x = a;
+
+    else if(x > z)
+
+    printf("%i %i %i",a, b, c);
+
+    return 0;
+}
