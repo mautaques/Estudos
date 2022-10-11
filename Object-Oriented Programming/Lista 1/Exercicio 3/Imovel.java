b4c829193a26033312ed18996431fe8717221038
Mauricio Taques <mtaquesfilho@gmail.com>
2022-10-10 23:28:04 -0300

    Add files via upload

---
+++ Object-Oriented Programming/Lista 1/Exercicio 3/Imovel.java
@@ -0,0 +1,42 @@
+package Exercicio3;
+
+public class Imovel {
+    private String nome;
+    private float largura;
+    private float comprimento;
+    private float preco;
+
+    public String getNome() {
+        return this.nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public float getLargura() {
+        return this.largura;
+    }
+
+    public void setLargura(float largura) {
+        this.largura = largura;
+    }
+
+    public float getComprimento() {
+        return this.comprimento;
+    }
+
+    public void setComprimento(float comprimento) {
+        this.comprimento = comprimento;
+    }
+
+    public float getPreco() {
+        return this.preco;
+    }
+
+    public void setPreco(float preco) {
+        this.preco = preco;
+    }
+
+    
+}
