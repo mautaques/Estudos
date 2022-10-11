cc27829f3d631cf9d31475cf08e26390d93b9b13
Mauricio Taques <mtaquesfilho@gmail.com>
2022-10-10 23:26:51 -0300

    Add files via upload

---
+++ Object-Oriented Programming/Lista 1/Exercicio 1_2/Cliente.java
@@ -0,0 +1,57 @@
+package Exercicio1e2;
+
+public class Cliente {
+    
+    private String nome;
+    private String cpf;
+    private String telefone;
+
+    public String getNome() {
+        return this.nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return this.cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getTelefone() {
+        return this.telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public Cliente(String nome, String cpf, String telefone){
+        this.nome = nome;
+        this.cpf = cpf;
+        this.telefone = telefone;
+    }
+
+    public String toString(){
+        return "Nome: "+ nome +"CPF: "+ cpf +"Telefone: "+ telefone;
+    } 
+
+    public boolean equals (Object objeto){
+        Cliente cliente;
+        if( ! (objeto instanceof Cliente)){
+            return false;
+        }
+        cliente = (Cliente) objeto;
+        if(nome.equals(cliente.getNome())){
+            return true;
+        }
+        return false;
+    }
+
+    public Cliente(){
+    }
+}
---
+++ Object-Oriented Programming/Lista 1/Exercicio 1_2/Veiculo.java
@@ -0,0 +1,58 @@
+package Exercicio1e2;
+
+public class Veiculo {
+
+    private String marca;
+    private int placa;
+    private String cor;
+
+    public String getMarca() {
+        return this.marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public int getPlaca() {
+        return this.placa;
+    }
+
+    public void setPlaca(int placa) {
+        this.placa = placa;
+    }
+
+    public String getCor() {
+        return this.cor;
+    }
+
+    public void setCor(String cor) {
+        this.cor = cor;
+    }
+
+    public Veiculo(String marca, int placa, String cor){
+        this.marca = marca;
+        this.placa = placa;
+        this.cor = cor;
+    }
+
+    public String toString(){
+        return "Marca: "+ marca +"Placa: "+ placa +"Cor: "+ cor;
+    } 
+
+    public boolean equals (Object objeto){
+        Veiculo veiculo;
+        if( ! (objeto instanceof Veiculo)){
+            return false;
+        }
+        veiculo = (Veiculo) objeto;
+        if(marca.equals(veiculo.getMarca())){
+            return true;
+        }
+        return false;
+    }
+
+    public Veiculo(){
+    }
+
+}
