8eae5d03d8a5c4a1d6692f830c61b00c5de0dc5d
Mauricio Taques <mtaquesfilho@gmail.com>
2022-10-10 21:35:34 -0300

    Add files via upload

---
+++ Object-Oriented Programming/Aula Pratica 2/Exercicio 2/dados/Endereço.java
@@ -0,0 +1,64 @@
+package Exercicio2;
+
+public class Endereço {
+    
+    private String rua;
+    private int numero;
+    private String bairro;
+    private String cidade;
+    private String estado;
+    private int cep;
+
+    public Endereço(){
+    }
+
+    public String getRua() {
+        return this.rua;
+    }
+
+    public void setRua(String rua) {
+        this.rua = rua;
+    }
+
+    public int getNumero(){
+        return this.numero;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    public String getBairro() {
+        return this.bairro;
+    }
+
+    public void setBairro(String bairro) {
+        this.bairro = bairro;
+    }
+
+    public String getCidade() {
+        return this.cidade;
+    }
+
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+
+    public String getEstado() {
+        return this.estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    public int getCep() {
+        return this.cep;
+    }
+
+    public void setCep(int cep) {
+        this.cep = cep;
+    }
+
+}
+
