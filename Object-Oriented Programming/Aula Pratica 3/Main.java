06a19f728d27b82e4f2ad38825d4b0de63c28aae
Mauricio Taques <mtaquesfilho@gmail.com>
2022-10-10 21:04:49 -0300

    Add files via upload

---
+++ Object-Oriented Programming/Aula Pratica 3/Main.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+	Scanner scan = new Scanner(System.in);
+
+	public void fazerReserva() {
+	}
+	
+	public void cadastrarCliente() {
+	}
+	
+	public void cadastrarCidade() {
+	}
+	
+	public void mostrarReservas() {
+	}
+	
+	public static void main(String[] args){
+		
+	}
+}
