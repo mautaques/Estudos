56f9eaf4efef33c742844d697fcd497eb2b79228
Mauricio Taques <mtaquesfilho@gmail.com>
2022-10-10 23:17:38 -0300

    Add files via upload

---
+++ Object-Oriented Programming/Aula Pratica 7/dados/Gerador.java
@@ -0,0 +1,22 @@
+package dados;
+
+public class Gerador {
+    
+    private int lista;
+
+    public int getLista() {
+        return this.lista;
+    }
+
+    public void setLista(int lista) {
+        this.lista = lista;
+    }
+
+    public gerar(){
+
+    }
+
+    public getSequencia(){
+        
+    }
+}
---
+++ Object-Oriented Programming/Aula Pratica 7/dados/ISequencia.java
@@ -0,0 +1,16 @@
+package dados;
+
+public class ISequencia {
+    
+    public int sortear();
+    public long somatorio();
+    public long produto();
+    public double mediaGeometria();
+    public double mediaAritmetica();
+    public double variancia();
+    public double desvioPadrao();
+    public int amplitude();
+
+    
+    
+}
