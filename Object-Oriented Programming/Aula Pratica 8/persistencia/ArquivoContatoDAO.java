9c52f148e958e84fdfa18b403de4644396504ffc
Mauricio Taques <mtaquesfilho@gmail.com>
2022-10-26 15:07:52 -0300

    Update ArquivoContatoDAO.java

--- Object-Oriented Programming/Aula Pratica 8/persistencia/ArquivoContatoDAO.java
+++ Object-Oriented Programming/Aula Pratica 8/persistencia/ArquivoContatoDAO.java
@@ -1,1 +1,31 @@
+public class ArquivoContatoDAO {
+
+    private String caminho;
+
+    public String getCaminho() {
+        return caminho;
+    }
+
+    public void setCaminho(String caminho) {
+        this.caminho = caminho;
+    }
+
+    private String toCSV(){
+    }
+
+    private Contato fromCSV(){
+    }
+
+    public Contato leContatos(){
+    }
+
+    public  salvaContatos(){
+    }
+
+    public String toString() {
+        return "ArquivoContatoDAO{" +
+                "caminho='" + caminho + '\'' +
+                '}';
+    }
+}
 
