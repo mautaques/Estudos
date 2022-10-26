2e21b811dba47856f7c9c4929da44411b68621a9
Mauricio Taques <mtaquesfilho@gmail.com>
2022-10-26 15:06:04 -0300

    Update Contato.java

--- Object-Oriented Programming/Aula Pratica 8/dados/Contato.java
+++ Object-Oriented Programming/Aula Pratica 8/dados/Contato.java
@@ -1,7 +1,36 @@
+import java.util.Objects;
 
-    private String name;
-    private int idade;
-    private float massa;
-    private float altura;
-    
-    
+public class Contato {
+
+    private String nome;
+    private int telefone;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(int telefone) {
+        this.telefone = telefone;
+    }
+
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Contato contato)) return false;
+        return telefone == contato.telefone && nome.equals(contato.nome);
+    }
+
+    public String toString() {
+        return "Contato{" +
+                "nome='" + nome + '\'' +
+                ", telefone=" + telefone +
+                '}';
+    }
+}
