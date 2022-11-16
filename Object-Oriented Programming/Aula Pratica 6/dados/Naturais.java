126fb2ff5712ef81acf0156ee0c67cf1aa07c383
Mauricio Taques <mtaquesfilho@gmail.com>
2022-11-16 00:24:16 -0300

    Delete Object-Oriented Programming/Aula Pratica 6 directory

--- Object-Oriented Programming/Aula Pratica 6/dados/Naturais.java
+++
@@ -1,9 +0,0 @@
-public class Naturais extends gerador{
-    
-    public void gerar(int n) {
-        for (int i = 0; i < n; i++) {
-            adicionarSequencia(n);
-        }
-    }
-
-}
--- Object-Oriented Programming/Aula Pratica 6/dados/quadrados.java
+++
@@ -1,9 +0,0 @@
-public class quadrados extends gerador{
-    
-    public void gerar(int x) {
-
-        for (int i = 0; i < x; i++) {
-            adicionarSequencia(i * i);
-        }
-    }
-}
