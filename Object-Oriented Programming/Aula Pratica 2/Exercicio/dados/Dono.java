c2b9f25408c6ebf0b4ff17c55da763b536e6d37b
Mauricio Taques <mtaquesfilho@gmail.com>
2022-11-16 00:02:38 -0300

    Create Dono.java

---
+++ Object-Oriented Programming/Aula Pratica 2/Exercicio/dados/Dono.java
@@ -0,0 +1,37 @@
+ackage Exercicio2;
+
+public class Dono {
+    
+    private String nome;
+    private Endereço endereco;
+    private int cpf;
+
+    public Dono(){
+    }
+
+    public String getNome() {
+        return this.nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Endereço getEndereco() {
+        return this.endereco;
+    }
+
+    public void setEndereco(Endereço endereco) {
+        this.endereco = endereco;
+    }
+
+    public int getCpf() {
+        return this.cpf;
+    }
+
+    public void setCpf(int cpf) {
+        this.cpf = cpf;
+    }
+
+
+}
