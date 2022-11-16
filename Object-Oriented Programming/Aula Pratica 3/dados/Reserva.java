42db656ca527f4899768c872d44e1497fb22e800
Mauricio Taques <mtaquesfilho@gmail.com>
2022-11-16 00:11:05 -0300

    Create Reserva.java

---
+++ Object-Oriented Programming/Aula Pratica 3/dados/Reserva.java
@@ -0,0 +1,56 @@
+public class Reserva {
+
+	private int numReserva;
+	private String dataVoo;
+	private String horaVoo;
+	private float preco;
+	private String classeVoo;
+	private Boolean idaEvolta;
+	private int poltrona;
+	
+	public int getNumReserva() {
+		return numReserva;
+	}
+	public void setNumReserva(int numReserva) {
+		this.numReserva = numReserva;
+	}
+	public String getDataVoo() {
+		return dataVoo;
+	}
+	public void setDataVoo(String dataVoo) {
+		this.dataVoo = dataVoo;
+	}
+	public String getHoraVoo() {
+		return horaVoo;
+	}
+	public void setHoraVoo(String horaVoo) {
+		this.horaVoo = horaVoo;
+	}
+	public float getPreco() {
+		return preco;
+	}
+	public void setPreco(float preco) {
+		this.preco = preco;
+	}
+	public String getClasseVoo() {
+		return classeVoo;
+	}
+	public void setClasseVoo(String classeVoo) {
+		this.classeVoo = classeVoo;
+	}
+	public Boolean getIdaEvolta() {
+		return idaEvolta;
+	}
+	public void setIdaEvolta(Boolean idaEvolta) {
+		this.idaEvolta = idaEvolta;
+	}
+	public int getPoltrona() {
+		return poltrona;
+	}
+	public void setPoltrona(int poltrona) {
+		this.poltrona = poltrona;
+	}
+	
+	
+	
+}
