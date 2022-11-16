fc5ab3e146fabe5390d97844eaccf2fdd8db0909
Mauricio Taques <mtaquesfilho@gmail.com>
2022-11-16 00:10:42 -0300

    Create Cliente.java

---
+++ Object-Oriented Programming/Aula Pratica 3/dados/Cliente.java
@@ -0,0 +1,40 @@
+public class Cliente {
+
+	private int cpf;
+	private String nome;
+	private String endereco;
+	private int telefone;
+
+	public int getCpf() {
+		return cpf;
+	}
+	public void setCpf(int cpf) {
+		this.cpf = cpf;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getEndereco() {
+		return endereco;
+	}
+	public void setEndereco(String endereco) {
+		this.endereco = endereco;
+	}
+	public int getTelefone() {
+		return telefone;
+	}
+	public void setTelefone(int telefone) {
+		this.telefone = telefone;
+	}
+		
+	public void reservarIda(){
+		return;
+	}
+	
+	public void reservarVolta() {
+	}
+	
+}
