d350f0913beaa75da49609f9d200f1776022a5a0
Mauricio Taques <mtaquesfilho@gmail.com>
2023-09-11 14:52:00 -0300

    Create SistemaGestao.java

---
+++ POO/Prova1/negocio/SistemaGestao.java
@@ -0,0 +1,28 @@
+import dados.Cliente;
+import dados.ClientePremium;
+import dados.Conta;
+
+public class SistemaGestao {
+    
+    List<Cliente> c = new ArrayList<Cliente> ();
+    List<ClientePremium> cpremium = new ArrayList<ClientePremium> ();
+    List<Conta> con = new ArrayList<Conta> ();
+
+    public adicionarCliente(Cliente c){
+        for(i = 0; i < 3; i++){
+            return c(i);
+        }
+    }
+
+    public adicionarClienteOuro(Cliente c){
+        for(i = 0; i < 3; i++){
+            return cpremium(i);
+        }
+    }
+
+    public adicionarConta(Conta conta, Cliente cliente){
+        for(i = 0; i < 3; i++){
+            return get
+        }
+    }
+}
