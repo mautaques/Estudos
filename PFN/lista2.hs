49a26a0589842e899f5bd8bb449e0376415f5109
Mauricio Taques <mtaquesfilho@gmail.com>
2023-10-10 16:09:46 -0300

    Update lista2.hs

--- PFN/lista2.hs
+++ PFN/lista2.hs
@@ -1,81 +1,67 @@
 -- Lista 2 Mauricio Martins Taques Filho
 
+-- Ex 1)
 pertence n [] = False
 pertence n (x:xs) = if n == x then True else pertence n xs
-   
+
+-- Ex 2)   
 intercessao [] ys = []
 intercessao (x:xs) (y:ys) = if pertence x (y:ys) then x:intercessao xs (y:ys) else intercessao xs (y:ys)
 
-inversoLista [] = []
-inversoLista (x:xs) = inversoLista xs ++ [x]
+-- Ex 3)
+inverso [] = []
+inverso (x:xs) = inversoLista xs ++ [x]
 
+-- Ex 4)
 nUltimos n [] = []
 nUltimos n (x:xs) = if n >= length (x:xs) then x:xs else nUltimos n xs
 
+-- Ex 5)
+soma2 [] = []
+soma2 (x:xs) = [a+b|(a,b) <- x:xs]
+
+-- Ex 6)
+
+
+-- Ex 7)
+intercalacao [] [] = []
+intercalacao (x:xs) (y:ys) = ordenarLista ((x:xs)++(y:ys))
+
+-- Ex 8)
+menor [x] = x
+menor (x:xs) = if x < menor xs then x else menor xs
+
+-- Ex 9)
+removerElem n [] = []
+removerElem n (x:xs) = if n == x then xs else x:removerElem n xs
+
+-- Ex 10)
+ordenar [] = []
+ordenar (x:xs) = menor (x:xs):ordenarLista (removerElem (menor(x:xs))(x:xs))
+
+-- Ex 11)
+ins n [] = [n]
+ins n (x:xs) = if pertence n (x:xs) then x:xs else ordenarLista(n : (x:xs))
+
+-- Ex 12)
 enesimo n [] = -1
 enesimo 1 [x] = x
 enesimo n (x:xs) = if n-1 == 0 then x else enesimo (n-1) xs
 
-repetir 0 _ = []
-repetir n m = [m] : repetir (n-1) m
-
-intercalacao [] [] = []
-intercalacao (x:xs) (y:ys) = ordenarLista ((x:xs)++(y:ys))
-
-menor [x] = x
-menor (x:xs) = if x < menor xs then x else menor xs
-
-removerElem n [] = []
-removerElem n (x:xs) = if n == x then xs else x:removerElem n xs
-
-ordenarLista [] = []
-ordenarLista (x:xs) = menor (x:xs):ordenarLista (removerElem (menor(x:xs))(x:xs))
-
-insereElem n [] = [n]
-insereElem n (x:xs) = if pertence n (x:xs) then x:xs else ordenarLista(n : (x:xs))
-
-primeirosDuplas [] = []
-primeirosDuplas ((a,b):xs) = a:primeirosDuplas xs
-
-somaDuplas [] = []
-somaDuplas (x:xs) = [a+b|(a,b) <- x:xs]
-
-menoresDuplas [] = []
-menoresDuplas (x:xs) = [(a,b)|(a,b) <- x:xs, a < b]
-
-separarDuplas n (x:xs) = ([y| y <- x:xs, y <= n], [w| w <- x:xs, w > n])
-
-mdc (a,b) 
- | a == b = a 
- | a > b = mdc (a - b , b) 
- | otherwise = mdc (a , b - a)
+-- Ex 13)
 
 
-inversoDupla [] = []
-inversoDupla (x:xs) = [(b,a)|(a,b) <- x:xs]
+-- Ex 14)
 
-simetrico (x:xs) = [a==b|(a,b) <- x:xs]
 
-pares 0 = []
-pares n = [(x,y)|x <- [1..n], y <- [1..n], y /= x]
+-- Ex 15)
 
-inverteDNA' 'A' = 'T'
-inverteDNA' 'T' = 'A'
-inverteDNA' 'C' = 'G'
-inverteDNA' 'G' = 'C'
 
-inverteDNA'' [] = []                  
-inverteDNA'' xs = map inverteDNA' xs
+-- Ex 16)
 
-inverteDNA xs = inversoLista (inverteDNA'' xs)
 
-trocoCafe' 0 _ = []
-trocoCafe' c [] = []
-trocoCafe' c (x:xs) = if div c x > 0 then (x, div c x):trocoCafe' (rem c x) (x:xs) else trocoCafe' c xs
+-- Ex 17)
 
-trocoCafe c p = trocoCafe' (p-c) [50,20,10,5]
+-- Ex 18)
 
-magica' (x:xs) 1 = [x]
-magica' (x:xs) n = repetir n x ++ magica' xs (n-1)
 
-magica xs = magica' xs (length xs) ++ tail(inversoLista (magica' xs (length xs))) 
