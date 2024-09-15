d273fd9a4a1546ce10ed9ee44e32624e808051cb
Mauricio Taques <mtaquesfilho@gmail.com>
2024-09-15 17:40:39 -0300

    Update fatorial.s

--- AOC/fatorial.s
+++ AOC/fatorial.s
@@ -7,9 +7,9 @@ main:
 	li $s1, 1
 	
 while:
-	mul $s1, $s1, $s0 # Multiplicando $s1 por $s0 -1 e atribuindo a $s1
+	mul $s1, $s1, $s0 # multiplicando $s1 por $s0 -1 e atribuindo a $s1
 	subi $s0, $s0, 1 # $s0 = $s0 - 1;
-	bnez $s0, while # Se $s0 != 0 então vá para while
+	bnez $s0, while # se $s0 != 0 então vá para while
 
 end:
 	li $v0, 10
