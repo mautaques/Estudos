6a5375772d10fc040cc617d52e8549b665ae00b8
Mauricio Taques <mtaquesfilho@gmail.com>
2024-09-15 17:43:38 -0300

    Update n_fatorial.s

--- AOC/n_fatorial.s
+++ AOC/n_fatorial.s
@@ -1,22 +1,22 @@
-#Fatorial de n
+#fatorial de n
 	
 .text
 .globl main
 
 main:
-	li $v0, 5 # Input do usuário
+	li $v0, 5 # input do usuário
 	syscall
-	addi $s0, $v0, 0 # Move $v0 para $s0
+	addi $s0, $v0, 0 # move $v0 para $s0
 	li $s1, 1
 	
 while:
-	#mul $s1 $s1 $s0 --- Multiplicando usando mul
-	mult $s0, $s1 # Multiplica $s0 e $s1 e aloca o resultado em lo
-	mflo $v1 # Move lo para $v1
-	addi $s1, $v1, 0 # Move $v1 para $s0 
+	#mul $s1 $s1 $s0 --- multiplicando usando mul
+	mult $s0, $s1 # multiplica $s0 e $s1 e aloca o resultado em lo
+	mflo $v1 # move lo para $v1
+	addi $s1, $v1, 0 # move $v1 para $s0 
 	subi $s0, $s0, 1 # $s0 = $s0 - 1;
-	bnez $s0, while # Se $s0 != 0 então vá para while
-	addi $a0, $s1, 0 # Move o valor a ser imprimido para $a0
+	bnez $s0, while # se $s0 != 0 então vá para while
+	addi $a0, $s1, 0 # move o valor a ser imprimido para $a0
 	li $v0, 1
 	syscall
 
