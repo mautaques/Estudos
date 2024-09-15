d6a1a50f46449dbf789d83ae8aebd19825516ebd
Mauricio Taques <mtaquesfilho@gmail.com>
2024-09-14 21:28:42 -0300

    Update ex_vetor2.s

--- AOC/ex_vetor2.s
+++ AOC/ex_vetor2.s
@@ -20,12 +20,12 @@ main:
 	lw $t1, 4($t0) #atribui o valor de v[1] para $t1
 	lw $t2, 8($t0) #atribui o valor de v[2] para $t2
 	lw $t3, 12($t0) #atribui o valor de v[3] para $t3
-	add $t2 $t2 $t1 #soma $t1 com $t2 e aloca em $t2
-	addi $t3 $t3 42 #soma $t3 com o imediato 42 e aloca em $t3
-	add $t3 $t3 $t2 #soma $t2 com $t3 e aloca em $t3
-	sw $t3 28($t0) #atribui o valor de $t3 para a posição v[7]
+	add $t2, $t2, $t1 #soma $t1 com $t2 e aloca em $t2
+	addi $t3, $t3, 42 #soma $t3 com o imediato 42 e aloca em $t3
+	add $t3, $t3, $t2 #soma $t2 com $t3 e aloca em $t3
+	sw $t3, 28($t0) #atribui o valor de $t3 para a posição v[7]
 	
 end:
-	li $v0 5
+	li $v0, 5
 	syscall	
 	
