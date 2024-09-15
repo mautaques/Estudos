ed70c8ec3bdb697fc1ec17954bc8b93ffde2818d
Mauricio Taques <mtaquesfilho@gmail.com>
2024-09-14 21:33:06 -0300

    Add files via upload

---
+++ AOC/fatorial.s
@@ -0,0 +1,16 @@
+#Fatorial de 9(9!)
+	
+	.text
+	.globl main
+main:
+	li $s0, 9
+	li $s1, 1
+	
+while:
+	mul $s1, $s1, $s0 # Multiplicando $s1 por $s0 -1 e atribuindo a $s1
+	subi $s0, $s0, 1 # $s0 = $s0 - 1;
+	bnez $s0, while # Se $s0 != 0 então vá para while
+
+end:
+	li $v0, 10
+	syscall
