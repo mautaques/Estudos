80be72112cb53bb5265742cfa71111670297f2e1
Mauricio Taques <mtaquesfilho@gmail.com>
2024-09-15 05:06:14 -0300

    Update soma_vetor_v2.s

--- AOC/soma_vetor_v2.s
+++ AOC/soma_vetor_v2.s
@@ -24,6 +24,11 @@ main:
 	addi $t3, $t3, 42 #soma $t3 com o imediato 42 e aloca em $t3
 	add $t3, $t3, $t2 #soma $t2 com $t3 e aloca em $t3
 	sw $t3, 28($t0) #atribui o valor de $t3 para a posição v[7]
+ 
+ print:
+	addi $a0, $t3, 0
+	li $v0, 1
+	syscall
 	
 end:
 	li $v0, 5
